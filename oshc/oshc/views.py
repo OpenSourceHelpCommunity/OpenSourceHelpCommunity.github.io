146cf29379a79864e7c797000c1979de9e7616d6
Ibrahim Jarif <jarifibrahim@gmail.com>
2017-06-01 16:12:42 +0530

    Upgrade to Django=1.11

--- oshc/oshc/views.py
+++ oshc/oshc/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 
 
-def home(request) :
-	return render_to_response('index.html', context_instance=RequestContext(request))
-
+def home(request):
+    return render(request, 'index.html')
