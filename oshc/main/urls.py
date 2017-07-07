c42733570430a7b6be8059241ba67a8b4394d9c8
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2017-07-07 18:51:47 +0530

    Added requesting a session

--- oshc/main/urls.py
+++ oshc/main/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name="home")
+    url(r'^$', views.home, name="home"),
+    url(r'^session/', views.request_session, name="request_session")
 ]
--- oshc/main/views.py
+++ oshc/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def request_session(request):
+    return render(request, 'session.html')
