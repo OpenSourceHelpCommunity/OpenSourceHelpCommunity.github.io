afa8dd3122de80c6e7737056b435d7183ed8d07d
Ibrahim Jarif <jarifibrahim@gmail.com>
2017-07-09 00:02:47 +0530

    tests.py: Add tests for Home view

--- oshc/main/tests.py
+++ oshc/main/tests.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeViewTests(SimpleTestCase):
+
+    def test_get_request(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
