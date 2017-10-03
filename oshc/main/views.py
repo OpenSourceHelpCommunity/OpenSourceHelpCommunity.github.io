e88dada9165036a58e73426bdec83a7d7c23da75
Tanish Bhatia <bhatia.tanish.96@gmail.com>
2017-10-03 18:32:02 +0530

    Custom Error Pages for 403, 404 and 500

--- oshc/main/views.py
+++ oshc/main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from django.shortcuts import render_to_response
+
+from django.template import RequestContext
+
 from main.models import chatSession
 
 
@@ -11,3 +15,24 @@ def home(request):
 
 def request_session(request):
     return render(request, 'session.html')
+
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
+
+
+def handler403(request):
+    response = render_to_response('403.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 403
+    return response
