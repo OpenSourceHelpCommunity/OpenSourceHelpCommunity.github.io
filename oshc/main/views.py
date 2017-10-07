5fab97135ea0098eedc200e62c09ffe5381a2b03
abhishekarya286 <abhishekarya286@gmail.com>
2017-10-08 00:23:22 +0530

    Resolved Travis CI and Hound CI errors

--- oshc/main/views.py
+++ oshc/main/views.py
@@ -5,6 +5,10 @@ from main.models import chatSession, Contest
 
 from datetime import datetime
 
+from django.shortcuts import render_to_response
+
+from django.template import RequestContext
+
 
 def home(request):
     session_list = chatSession.objects.order_by('start_date')[:3]
@@ -41,3 +45,24 @@ def add_contest(request):
     contest.save()
 
     return HttpResponseRedirect("/contests/")
+
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
+
+
+def handler403(request):
+    response = render_to_response('403.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 403
+    return response
